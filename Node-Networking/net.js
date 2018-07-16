3ff49a12bbfe05a1836b7271da4c35b84f634646
diego.meneses <diego.meneses@pragma.com.co>
2018-07-16 14:13:00 -0500

    chat web con eventos

--- Node-Networking/net.js
+++ Node-Networking/net.js
@@ -4,23 +4,38 @@ const server = require('net').createServer();
 let counter = 0;
 let sockets = {};
 
+function timestamp() {
+  const now = new Date();
+  return `${now.getHours()}:${now.getMinutes()}}`;
+}
+
 server.on('connection', socket => {
   socket.id = counter++;
-  sockets[socket.id] = socket;
+
 
   console.log('client connected :)');
-  socket.write('welcome new client!\n');
+  socket.write('Prese type your name!');
 
   socket.on('data', data => {
-    socket.write(`${socket.id}: `);
-    socket.write(data);
+    if(!sockets[socket.id]) {
+      socket.name = data.toString().trim();
+      socket.write(`Welcome ${socket.name}!\n`);
+      sockets[socket.id] = socket;
+      return;
+    }
+    Object.entries(sockets).forEach(([key, cs]) => {
+      if (socket.id == key ) return;
+      cs.write(`${socket.name}: ${timestamp()}`);
+      cs.write(data + '\n\r');
+    });
   });
 
   socket.on('end', () => {
+    delete sockets[socket.id];
     console.log('Client disconnected :(');
   })
 
-  socket.setEncoding('utf8');
+ // socket.setEncoding('utf8');
 })
 
 server.listen(23, () => console.log('server bound'));
